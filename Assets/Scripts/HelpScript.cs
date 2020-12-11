9d3b4582ecbe6caa4a6fbb2b8c72a4b837b528fb
shanetomlinson <tiedupinside@gmail.com>
2020-12-11 07:59:51 -0700

    Initial commit, moved project from branch to its own repo

---
+++ Assets/Scripts/HelpScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpScript : MonoBehaviour
+{
+    public AudioSource Sound1;
+    public AudioSource Sound2;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Sound1.Play();
+        Sound2.Play();
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/RoomTransitionScripts.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class RoomTransitionScripts : MonoBehaviour
+{
+    public AudioMixerSnapshot InRoom;
+    public AudioMixerSnapshot OutOfRoom;
+  
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "InRoom")
+        {
+            InRoom.TransitionTo(0.5f);
+        }
+        if (other.gameObject.tag == "OutOfRoom")
+        {
+            OutOfRoom.TransitionTo(0.5f);
+        }
+    }
+}
